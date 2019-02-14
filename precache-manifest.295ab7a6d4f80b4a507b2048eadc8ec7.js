7a8cbde36a36cf5b0c8321cae4658a1022ef9f37
Laza <postlaza@gmail.com>
2019-02-13 23:11:41 -0800

    Updates

---
+++ precache-manifest.295ab7a6d4f80b4a507b2048eadc8ec7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "78666339e2788767edfd",
+    "url": "/clicky-game/static/js/main.78666339.chunk.js"
+  },
+  {
+    "revision": "ba2021e67c053dfd7e1d",
+    "url": "/clicky-game/static/js/2.ba2021e6.chunk.js"
+  },
+  {
+    "revision": "78666339e2788767edfd",
+    "url": "/clicky-game/static/css/main.768a7196.chunk.css"
+  },
+  {
+    "revision": "3afc9763dfea1498c7fbd43044bf8620",
+    "url": "/clicky-game/index.html"
+  }
+];
